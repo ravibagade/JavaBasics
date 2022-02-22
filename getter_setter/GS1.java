083df268dad1c9ed6bb369a591a2fe85c486ba0a
Ravi <ravibagade512@gmail.com>
2022-02-23 00:24:50 +0530

    new folder

---
+++ getter_setter/GS1.java
@@ -0,0 +1,15 @@
+class  
+{
+   String name:
+    int rno;
+  public void display()
+{  
+   System.out.println(this.name);
+   System.out.println(this.rno);
+	 
+}
+ 	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+	}
+}
